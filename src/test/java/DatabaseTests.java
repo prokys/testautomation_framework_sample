bb66e4ba2f670004de05e4e7156b6633ef2587f7
Prokop Ferdan <prokes1@gmail.com>
2024-06-05 21:27:00 +0200

    updated database tests and added before and after methods for setup and cleanup

--- src/test/java/DatabaseTests.java
+++ src/test/java/DatabaseTests.java
@@ -1,78 +1,80 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import utils.PostgresUtils;
 import utils.PropertiesUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
 public class DatabaseTests {
     int affectedRows;
+    Response response;
+
+    String petType = "petType";
+    String petTypeEdited = "petTypeEdited";
 
     @BeforeTest
     public void connectToDb(){
         PropertiesUtils.loadEnvironmentConfiguration();
         PostgresUtils.connectToDb();
+        System.out.println("Before test");
     }
     @AfterTest
     public void disconnectFromDb(){
         PostgresUtils.disconnectFromDb();
+        System.out.println("After test");
     }
-    @DataProvider(name = "existingPetType")
-    public Object[][] existingPetType() {
-        return new Object[][] {
-                {"dog"},
-        };
-    }
-    @Test(dataProvider = "existingPetType")
-    public void checkIfPetTypeIsInDatabase(String petType) {
-        ArrayList<HashMap<String,String>> petTypes = PostgresUtils.executeQuery("SELECT * FROM types WHERE name='"+petType+"'");
 
-        if (petTypes.size()==0){
-            System.out.println("Pet type "+petType+ " is not in database");
-        } else if (petTypes.get(0).containsValue(petType)) {
-            System.out.println("Pet type "+petType+ " is in database");
-        } else  {
-            System.out.println("Something went wrong");
-        }
+    @Test(groups = "createPetTypeBeforeDeleteAfter")
+    public void checkIfPetTypeIsInDatabase() {
+        ArrayList<HashMap<String,String>> petTypes = PostgresUtils.executeQuery("SELECT * FROM types WHERE name='"+petType+"'");
+        Assert.assertTrue(petTypes.get(0).containsValue(petType));
+
+        System.out.println("Pet type "+petType+" is in database");
     }
-    @DataProvider(name = "newPetType")
-    public Object[][] newPetType() {
-        return new Object[][] {
-                {"monkey"},
-        };
-    }
-    @Test(dataProvider = "newPetType")
-    public void createNewPetType(String petType) {
+    @Test(groups = "deletePetTypeAfter")
+    public void createNewPetType() {
         affectedRows = PostgresUtils.executeStatement("INSERT INTO types (name) VALUES ('"+petType+"')");
         Assert.assertEquals(affectedRows,1);
         System.out.println("Pet type "+petType+ " added into database");
     }
-    @DataProvider(name = "oldAndNewPetType")
-    public Object[][] oldAndNewPetType() {
-        return new Object[][] {
-                {"monkey", "fish"},
-        };
-    }
-    @Test(dataProvider = "oldAndNewPetType")
-    public void updatePetTypeTo(String oldPetType, String newPetType) {
-        affectedRows = PostgresUtils.executeStatement("UPDATE types SET name='" + newPetType + "' WHERE name='" + oldPetType + "'");
+    @Test(groups = "createPetTypeBeforeDeleteAfter")
+    public void updatePetTypeTo() {
+        affectedRows = PostgresUtils.executeStatement("UPDATE types SET name='" + petTypeEdited + "' WHERE name='" + petType + "'");
         Assert.assertEquals(affectedRows, 1);
-        System.out.println("Pet type " + oldPetType + " updated to " + newPetType);
+        System.out.println("Pet type " + petType + " updated to " + petTypeEdited);
     }
-    @DataProvider(name = "editedPetType")
-    public Object[][] editedPetType() {
-        return new Object[][] {
-                {"fish"},
-        };
-    }
-    @Test(dataProvider = "editedPetType")
-    public void deletePetTypeFromDatabase(String petType) {
+    @Test(groups = "createPetTypeBeforeDeleteAfter")
+    public void deletePetTypeFromDatabase() {
         affectedRows = PostgresUtils.executeStatement("DELETE FROM types WHERE name='"+petType+"'");
         Assert.assertEquals(affectedRows,1);
         System.out.println("Pet type "+petType+ " deleted from database");
+    }
+
+    @BeforeMethod(onlyForGroups = "createPetTypeBeforeDeleteAfter", inheritGroups = false)
+    public void beforeMethod(){
+        String requestBody = "{\"name\": \""+petType+"\"}";
+        response = given().contentType(ContentType.JSON).body(requestBody).when().post("http://localhost:9966/petclinic/api/pettypes");
+        System.out.println("Before method");
+    }
+    @AfterMethod(onlyForGroups = {"createPetTypeBeforeDeleteAfter", "deletePetTypeAfter"}, inheritGroups = false)
+    public void afterMethod(){
+        response = when().get("http://localhost:9966/petclinic/api/pettypes");
+        ArrayList<String> petTypes = response.then().extract().body().path( "name");
+        ArrayList<Integer> petTypesIDs = response.then().extract().body().path( "id");
+        int petTypeID;
+        if(petTypes.contains(petType)){
+            petTypeID = (petTypesIDs.get(petTypes.indexOf(petType)));
+            when().delete("http://localhost:9966/petclinic/api/pettypes/"+petTypeID);
+        } else if (petTypes.contains(petTypeEdited)) {
+            petTypeID = (petTypesIDs.get(petTypes.indexOf(petTypeEdited)));
+            when().delete("http://localhost:9966/petclinic/api/pettypes/"+petTypeID);
+        }
+        System.out.println("After method");
     }
 }
