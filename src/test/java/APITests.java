a74de415ab3277edfe5968aa6b746db56eb69110
Prokop Ferdan <prokes1@gmail.com>
2024-06-05 18:00:18 +0200

    edited API tests, added before and after methods for setup and cleanup

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -1,10 +1,9 @@
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
-import java.util.ArrayList;
+import org.testng.annotations.*;
+import utils.PostgresUtils;
+import utils.PropertiesUtils;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
@@ -15,70 +14,66 @@ public class APITests {
     public int vetID;
     public Response response;
 
-    @DataProvider(name = "existingVetData")
-    public Object[][] existingVetData() {
-        return new Object[][] {
-                { "Helen", "Leary", "radiology"},
-        };
-    }
-    @Test(dataProvider = "existingVetData")
-    public void checkIfVetIsInDatabase(String firstName, String lastName, String specialty) {
-        if(vetID==0) {
-            ArrayList<String> vetsFirstNames = when().get(vetsAPIUrl).then().extract().path("firstName");
-            ArrayList<String> vetsLastNames = when().get(vetsAPIUrl).then().extract().path("lastName");
-            ArrayList<Integer> vetsIDs = when().get(vetsAPIUrl).then().extract().path("id");
+    String firstName = "firstName";
+    String lastName = "lastName";
+    String firstNameEdited = "firstNameEdited";
+    String lastNameEdited = "lastNameEdited";
 
-            if (vetsFirstNames.indexOf(firstName) == vetsLastNames.indexOf(lastName)) {
-                vetID = vetsIDs.get(vetsFirstNames.indexOf(firstName));
-            }
-        }
+
+    @BeforeTest
+    public void beforeTest(){
+        PropertiesUtils.loadEnvironmentConfiguration();
+        System.out.println("Before test");
+    }
+
+    @Test(groups = "createVetBeforeDeleteAfter")
+    public void checkIfVeterinarianIsInDatabase() {
         response = when().get(vetsAPIUrl + "/" + vetID);
         if (response.then().extract().statusCode()==200) {
             Assert.assertEquals(response.then().extract().path("firstName"), firstName);
             Assert.assertEquals(response.then().extract().path("lastName"), lastName);
-            Assert.assertEquals(response.then().extract().path("specialties.name[0]"), specialty);
-            System.out.println("User "+firstName+" "+ lastName+" is in database");
-        } else {
-            System.out.println("User "+firstName+" "+ lastName+" is not in database");
+//            Assert.assertEquals(response.then().extract().path("specialties.name[0]"), specialty);
+            System.out.println("Veterinarian "+firstName+" "+ lastName+" is in database");
         }
     }
 
-    @DataProvider(name = "newVetData")
-    public Object[][] newVetData() {
-        return new Object[][] {
-                { "firstName", "lastName", "surgery"},
-        };
-    }
-    @Test(dataProvider = "newVetData")
-    public void addVeterinarian(String firstName, String lastName, String specialty) {
-        String requestBody = "{\"firstName\": \""+firstName+"\", \"lastName\": \""+lastName+"\", \"specialties\": [{\"name\": \""+specialty+"\"}]}";
+    @Test(groups = "deleteVetAfter")
+    public void addVeterinarian() {
+        String requestBody = "{\"firstName\": \""+firstName+"\", \"lastName\": \""+lastName+"\", \"specialties\": [{\"name\": \" \"}]}";
         response = given().contentType(ContentType.JSON).body(requestBody).when().post(vetsAPIUrl);
         Assert.assertEquals(response.then().extract().statusCode(), 201);
-        vetID = response.then().extract().path("id");
-        System.out.println("User "+firstName+" "+lastName+" added to database");
-        checkIfVetIsInDatabase(firstName, lastName, specialty);
+        System.out.println("Veterinarian "+firstName+" "+lastName+" added to database");
     }
 
-    @DataProvider(name = "editVetData")
-    public Object[][] editVetData() {
-        return new Object[][] {
-                { "firstName", "lastName", "surgery", "firstNameEdited", "lastNameEdited", "surgeryEdited"},
-        };
-    }
-
-    @Test(dataProvider = "editVetData")
-    public void editVeterinarianToWithSpecialty(String firstName, String lastName, String specialty, String firstNameEdited, String lastNameEdited, String specialtyEdited) {
-        checkIfVetIsInDatabase(firstName, lastName, specialty);
-        String requestBody = "{\"firstName\": \""+firstNameEdited+"\", \"lastName\": \""+lastNameEdited+"\", \"specialties\": [{\"name\": \""+specialtyEdited+"\"}]}";
+    @Test(groups = "createVetBeforeDeleteAfter")
+    public void editVeterinarian() {
+        String requestBody = "{\"firstName\": \""+firstNameEdited+"\", \"lastName\": \""+lastNameEdited+"\", \"specialties\": [{\"name\": \" \"}]}";
         response = given().contentType(ContentType.JSON).body(requestBody).when().put(vetsAPIUrl+"/"+vetID);
         Assert.assertEquals(response.then().extract().statusCode(), 204);
-        System.out.println("User edited to: "+firstNameEdited+" "+lastNameEdited);
+        System.out.println("Veterinarian edited to "+firstNameEdited+" "+lastNameEdited);
     }
-    @Test(dataProvider = "newVetData")
-    public void deleteVeterinarianWithSpecialtyFromDatabase(String firstName, String lastName, String specialty) {
-        checkIfVetIsInDatabase(firstName, lastName, specialty);
+    @Test(groups = "createVetBeforeDeleteAfter")
+    public void deleteVeterinarian() {
         response = when().delete(vetsAPIUrl+"/"+vetID);
         Assert.assertEquals(response.then().extract().statusCode(), 204);
-        System.out.println("User"+firstName+" "+lastName+" with specialty "+specialty+" deleted from database");
+        System.out.println("Veterinarian "+firstName+" "+lastName+" deleted from database");
+    }
+
+    @BeforeMethod(onlyForGroups = "createVetBeforeDeleteAfter", inheritGroups = false)
+    public void beforeMethod(){
+        PostgresUtils.connectToDb();
+        PostgresUtils.executeStatement("INSERT INTO vets (first_name, last_name) VALUES ('"+firstName+"', '"+lastName+"')");
+        vetID = Integer.parseInt(PostgresUtils.executeQuery("SELECT id FROM vets WHERE (first_name='"+firstName+"') AND (last_name='"+lastName+"')").get(0).get("id"));
+        PostgresUtils.disconnectFromDb();
+        System.out.println("Before method");
+    }
+
+    @AfterMethod(onlyForGroups = {"createVetBeforeDeleteAfter", "deleteVetAfter"}, inheritGroups = false)
+    public void afterMethod(){
+        PostgresUtils.connectToDb();
+        PostgresUtils.executeStatement("DELETE FROM vets WHERE (first_name='"+firstName+"') AND (last_name='"+lastName+"')");
+        PostgresUtils.executeStatement("DELETE FROM vets WHERE (first_name='"+firstNameEdited+"') AND (last_name='"+lastNameEdited+"')");
+        PostgresUtils.disconnectFromDb();
+        System.out.println("After method");
     }
 }
